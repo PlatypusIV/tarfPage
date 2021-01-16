028375f81caea1c1fe59e1e81c7b6a3a2709aff1
unknown <hkymera2@gmail.com>
2021-01-16 16:50:22 +0200

    removed useless footer stuff, fixed title and icon.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,8 +7,8 @@ export default class Footer extends react.Component {
         return (<div>
             <footer id="footer">
                 <div class="inner">
-                    <h2>Etiam veroeros lorem</h2>
-                    <p>Pellentesque eleifend malesuada efficitur. Curabitur volutpat dui mi, ac imperdiet dolor tincidunt nec. Ut erat lectus, dictum sit amet lectus a, aliquet rutrum mauris. Etiam nec lectus hendrerit, consectetur risus viverra, iaculis orci. Phasellus eu nibh ut mi luctus auctor. Donec sit amet dolor in diam feugiat venenatis. </p>
+                    {/* <h2>Etiam veroeros lorem</h2>
+                    <p>Pellentesque eleifend malesuada efficitur. Curabitur volutpat dui mi, ac imperdiet dolor tincidunt nec. Ut erat lectus, dictum sit amet lectus a, aliquet rutrum mauris. Etiam nec lectus hendrerit, consectetur risus viverra, iaculis orci. Phasellus eu nibh ut mi luctus auctor. Donec sit amet dolor in diam feugiat venenatis. </p> */}
 
                     <ul class="icons">
                         <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
@@ -16,7 +16,7 @@ export default class Footer extends react.Component {
                         <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
                         <li><a href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
                     </ul>
-                    <p class="copyright">&copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com/">Unsplash</a>. Videos: <a href="http://coverr.co/">Coverr</a>.</p>
+                    <p class="copyright">&copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Reactified by: : <a href="https://github.com/PlatypusIV"><strong>Herbert-Ken Ümera</strong></a></p>
                 </div>
             </footer>
         </div>)
