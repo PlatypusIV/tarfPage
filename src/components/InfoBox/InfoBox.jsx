5958093e51dc980d6d8f386580296de85cd1b551
unknown <hkymera2@gmail.com>
2021-01-20 01:18:16 +0200

    added 2 more images to img folder

--- src/components/InfoBox/InfoBox.jsx
+++ src/components/InfoBox/InfoBox.jsx
@@ -3,20 +3,12 @@ import "./InfoBox.css";
 
 class InfoBox extends react.Component {
 
+
     render() {
         return (<div className="divMainInfo">
-            <span>
-                <h1><strong>Introduction</strong></h1>
-            </span>
-            <span>
-                <p>
-                    Hi. Im Tarvi. Im a musician, youtuber and a big guy with a big heart.<br/>
-
-                </p>
-            </span>
-            <div className="divSupportMe">
-
-            </div>
+            <p>Hi I'm Tarf</p>
+            <p>And I make metal Covers of anime/Video game/Vtuber music.</p><p> I also have a bunch of original stuff with either my <a href="https://open.spotify.com/artist/6vg2jNAYHgIg3FfWLv7EoG"><strong>band</strong></a> or solo artist.
+    Check em out and hope you enjoy!</p>
         </div>)
     }
 }
