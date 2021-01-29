45caacf94b638846ad081617cb64c49189a4dcd2
unknown <hkymera2@gmail.com>
2021-01-30 01:57:58 +0200

    increased letter spacing

--- src/components/InfoBox/InfoBox.jsx
+++ src/components/InfoBox/InfoBox.jsx
@@ -7,7 +7,7 @@ class InfoBox extends react.Component {
         return (<div className="divMainInfo">
             <div className="divMainText">
                 <p>Hi I'm Tarf</p>
-                <p>And I make metal Covers of anime/Video game/Vtuber music.</p><p> I also have a bunch of original stuff with either my <a href="https://open.spotify.com/artist/6vg2jNAYHgIg3FfWLv7EoG"><strong>band</strong></a> or solo artist.</p><p>Check em out and hope you enjoy!</p>
+                <p>And I make metal Covers of anime/ Video game/ Vtuber music.</p><p> I also have a bunch of original stuff with either my <a href="https://open.spotify.com/artist/6vg2jNAYHgIg3FfWLv7EoG"><strong>band</strong></a> or solo artist.</p><p>Check em out and hope you enjoy!</p>
             </div>
 
             <div className="divMainImage">
